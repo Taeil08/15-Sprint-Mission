41b1be1e70fcf973d0c6e965c410020fc57d124d
김태일 <ejrrhcl56@naver.com>
2025-05-15 18:44:51 +0900

    미션5,6,7 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import Header from "./component/Header";
-import Hero from "./component/Hero";
-import HotItems from "./component/HotItems";
-import Register from "./component/Register";
-import Search from "./component/Search";
-import Trust from "./component/Trust";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "@/pages/Home";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/LogIn/Components/SignUp";
+import SignIn from "./pages/LogIn/Components/SignIn";
 import "./App.css";
+import Items from "./pages/Items";
+import AddItem from "./pages/AddItem";
+import Product from "./pages/Items/Product";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Hero />
-      <HotItems />
-      <Search />
-      <Register />
-      <Trust />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/LogIn" element={<LogIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/Items" element={<Items />} />
+        <Route path="/AddItem" element={<AddItem />} />
+        <Route path="/Product" element={<Product />} />
+        <Route path="/Product/:productId" element={<Product />} />
+      </Routes>
+    </Router>
   );
 }
 
